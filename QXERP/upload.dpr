18ab4fcc2d1c6985321e842c7591d9396060e5ea
blueops1 <hyzfgjj@163.com>
2015-11-17 16:45:04 +0800

    add auto update module

---
+++ QXERP/upload.dpr
@@ -0,0 +1,14 @@
+program upload;
+
+uses
+  Forms,
+  uploadpatch in 'uploadpatch.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
